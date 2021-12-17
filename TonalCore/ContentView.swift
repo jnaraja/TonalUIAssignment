f2c9ddd241c935fbd1aa29b47f40064b4d48ed32
Jon-Jon <j2naraja@gmail.com>
2021-12-16 19:09:30 -0800

    Initial Commit

---
+++ TonalCore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TonalCore
+//
+//  Created by Jonathan Naraja on 12/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TonalCore/TonalCoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  TonalCoreApp.swift
+//  TonalCore
+//
+//  Created by Jonathan Naraja on 12/16/21.
+//
+
+import SwiftUI
+
+@main
+struct TonalCoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
